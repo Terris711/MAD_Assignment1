b0ae4875768ca47922a6032eaf83c30269a2104a
Terris711 <terristerris200501@gmail.com>
2023-08-29 23:32:27 +0800

    Fragment Implementation Layout

--- app/src/main/java/com/example/tictactoe/InGameFragment.java
+++ app/src/main/java/com/example/tictactoe/InGameFragment.java
@@ -59,6 +59,6 @@ public class InGameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_in_game, container, false);
+        return inflater.inflate(R.layout.fragment_33_in_game, container, false);
     }
 }
