25073ee6e19b8fabd5441de99806722471390f67
Tai Ha <u7454578@anu.edu.au>
2023-08-30 02:03:29 +1000

    amen

--- app/src/main/java/com/example/tictactoe/BoardGameFragment4x4.java
+++ app/src/main/java/com/example/tictactoe/BoardGameFragment4x4.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link InGameFragment#newInstance} factory method to
+ * Use the {@link BoardGameFragment4x4#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class InGameFragment extends Fragment {
+public class BoardGameFragment4x4 extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class InGameFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public InGameFragment() {
+    public BoardGameFragment4x4() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class InGameFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment InGameFragment.
+     * @return A new instance of fragment BoardGameFragment4x4.
      */
     // TODO: Rename and change types and number of parameters
-    public static InGameFragment newInstance(String param1, String param2) {
-        InGameFragment fragment = new InGameFragment();
+    public static BoardGameFragment4x4 newInstance(String param1, String param2) {
+        BoardGameFragment4x4 fragment = new BoardGameFragment4x4();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class InGameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_33_in_game, container, false);
+        return inflater.inflate(R.layout.fragment_board_game_fragment4x4, container, false);
     }
 }
--- app/src/main/java/com/example/tictactoe/InGameActivity.java
+++ app/src/main/java/com/example/tictactoe/InGameActivity.java
@@ -1,14 +1,23 @@
 package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class InGameActivity extends AppCompatActivity {
-
+    BoardGameFragment3x3 grid3x3 = new BoardGameFragment3x3();
+    BoardGameFragment4x4 grid4x4 = new BoardGameFragment4x4();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_game);
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction().add(R.id.grid_fragment, grid4x4).commit();
+
     }
 }
